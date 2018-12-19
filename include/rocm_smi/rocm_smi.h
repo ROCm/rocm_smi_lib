5e6424cab32dab934964f7134ff5cbbb9cf859d1
Chris Freehill <Chris.Freehill@amd.com>
2018-12-19 17:24:27 -0600

    Handle case where PCIe information is not implemented in system
    Also add a new error code for this.

--- include/rocm_smi/rocm_smi.h
+++ include/rocm_smi/rocm_smi.h
@@ -84,9 +84,14 @@ typedef enum {
   RSMI_STATUS_INTERNAL_EXCEPTION,        //!< An internal exception was caught
   RSMI_STATUS_INPUT_OUT_OF_BOUNDS,       //!< The provided input is out of
                                          //!< allowable or safe range
-  RSMI_INITIALIZATION_ERROR,             //!< An error occurred when rsmi
+  RSMI_STATUS_INIT_ERROR,                //!< An error occurred when rsmi
                                          //!< initializing internal data
                                          //!< structures
+  RSMI_INITIALIZATION_ERROR = RSMI_STATUS_INIT_ERROR,
+  RSMI_STATUS_NOT_YET_IMPLEMENTED,       //!< The requested function has not
+                                         //!< yet been implemented in the
+                                         //!< current system for the current
+                                         //!< devices
   RSMI_STATUS_UNKNOWN_ERROR = 0xFFFFFFFF,  //!< An unknown error occurred
 } rsmi_status_t;
 
