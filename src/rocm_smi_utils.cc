80d850ad2c642c62af3a9a608817c5d38e4f4458
Chris Freehill <Chris.Freehill@amd.com>
2019-11-04 13:45:47 -0600

    Docs., error checking and test improvements
    * Update doc. on api-support function
    * Check for valid integer value when reading a monitor int. val.
    * If fan-write test attempts to set speed higher than max.
       possible, then skip the test
    
    Change-Id: I01ad0ab1f4caffdb0d2c26e9575f278c35a6b017
    
    
    [ROCm/rocm_smi_lib commit: 52dfa4bccab015bd6a53704f50f7f53957c555cc]

--- src/rocm_smi_utils.cc
+++ src/rocm_smi_utils.cc
@@ -140,5 +140,17 @@ int ReadSysfsStr(std::string path, std::string *retStr) {
   return ret;
 }
 
+bool IsInteger(const std::string & n_str)
+{
+   if(n_str.empty() || ((!isdigit(n_str[0])) && (n_str[0] != '-')
+                                                      && (n_str[0] != '+'))) {
+     return false;
+   }
+
+   char * tmp;
+   strtol(n_str.c_str(), &tmp, 10);
+
+   return (*tmp == 0);
+}
 }  // namespace smi
 }  // namespace amd
