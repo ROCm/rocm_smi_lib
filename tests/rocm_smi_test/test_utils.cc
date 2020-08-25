a05200d2481097fedab72ed5e9543e7a261fb957
Mukul Joshi <mukul.joshi@amd.com>
2020-08-24 22:28:19 -0400

    Add support for GPU reset SMI events
    Add handling for both pre GPU reset and post GPU reset SMI
    events.
    
    Change-Id: I64d5e006bef58cb28b1c580c75f482a4590427da

--- tests/rocm_smi_test/test_utils.cc
+++ tests/rocm_smi_test/test_utils.cc
@@ -81,6 +81,8 @@ static const std::map<rsmi_evt_notification_type_t, const char *>
                                                       kEvtNotifEvntNameMap = {
     {RSMI_EVT_NOTIF_VMFAULT, "RSMI_EVT_NOTIF_VMFAULT"},
     {RSMI_EVT_NOTIF_THERMAL_THROTTLE, "RSMI_EVT_NOTIF_THERMAL_THROTTLE"},
+    {RSMI_EVT_NOTIF_GPU_PRE_RESET, "RSMI_EVT_NOTIF_GPU_PRE_RESET"},
+    {RSMI_EVT_NOTIF_GPU_POST_RESET, "RSMI_EVT_NOTIF_GPU_POST_RESET"},
 };
 const char *
 NameFromEvtNotifType(rsmi_evt_notification_type_t evt) {
