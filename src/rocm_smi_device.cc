558b3c38f1067c85231d28d3c8f1560f221cfa6a
Chris Freehill <Chris.Freehill@amd.com>
2020-10-05 09:33:40 -0500

    Support for RSMI_EVNT_GRP_XGMI_DATA_OUT counters
    Also some format fixes
    
    Change-Id: Id3c0f6b3cf5b327bb9ca6acb6091dc67764c8032
    
    
    [ROCm/rocm_smi_lib commit: 946bf93dfb31c26527ce37ce516f734e3ca87775]

--- src/rocm_smi_device.cc
+++ src/rocm_smi_device.cc
@@ -641,7 +641,7 @@ int Device::readDevInfoBinary(DevInfoTypes type,
   }
   // copies all data into buffer
   retVec->insert(retVec->begin(),
-               std::istreambuf_iterator<char>(fs),{});
+               std::istreambuf_iterator<char>(fs), {});
 
   return 0;
 }
