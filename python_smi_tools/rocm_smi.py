ROCm/rocm_smi_lib

commit c7c2ac55597915cc2584ebf206d5c5d55a7c4833

rocm_smi.py: Don't try to reset non-AMD GPUs
This won't work for obvious reasons, so exit with an error instead of
trying to access a file that doesn't exist and segfaulting

Change-Id: Id1230922fa6e9a19e9394280faad88a43c7d2e34

python_smi_tools/rocm_smi.py
@@ -941,6 +941,10 @@ def resetGpu(device):
         logging.error('GPU Reset can only be performed on one GPU per call')
         RETCODE = 1
         return
+    if not isAmdDevice(device):
+        logging.error('GPU Reset can only be performed on an AMD GPU')
+        RETCODE = 1
+        return
     resetDev = int(device[0])
     ret = rocmsmi.rsmi_dev_gpu_reset(resetDev)
     if rsmi_ret_ok(ret, resetDev):

commit 242d94a668e40ad77d47688d6b6787aed269de41

rocm_smi.py: Fix gpu reset error
Since device is a list, we need to pass a single item to the isAmdGpu
function.

Fixes: c7c2ac555979 "rocm_smi.py: Don't try to reset non-AMD GPUs"

Signed-off-by: Kent Russell <kent.russell@amd.com>
Change-Id: I19a74377636ff4589f11d092f41e1d35c1acb307

python_smi_tools/rocm_smi.py
@@ -961,11 +961,11 @@ def resetGpu(device):
         logging.error('GPU Reset can only be performed on one GPU per call')
         RETCODE = 1
         return
-    if not isAmdDevice(device):
+    resetDev = int(device[0])
+    if not isAmdDevice(resetDev):
         logging.error('GPU Reset can only be performed on an AMD GPU')
         RETCODE = 1
         return
-    resetDev = int(device[0])
     ret = rocmsmi.rsmi_dev_gpu_reset(resetDev)
     if rsmi_ret_ok(ret, resetDev):
         printLog(resetDev, 'Successfully reset GPU %d' % (resetDev), None)