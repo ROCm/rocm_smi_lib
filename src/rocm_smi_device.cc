838b7044edb6a557941a6d1052b3bd146c7b360e
Galantsev, Dmitrii <dmitrii.galantsev@amd.com>
2023-09-11 19:23:46 -0500

    SWDEV-409184 - Fix erroneous 'not supported' when HWMON is absent
    Change-Id: Ic5ff406977d962fadc709a03853dac61b5460a26
    Signed-off-by: Galantsev, Dmitrii <dmitrii.galantsev@amd.com>
    (cherry picked from commit 41ade41d8467eadc37ab60c4a423a1ece9a65449)

--- src/rocm_smi_device.cc
+++ src/rocm_smi_device.cc
@@ -1092,10 +1092,6 @@ void Device::fillSupportedFuncs(void) {
   if (supported_funcs_.size() != 0) {
     return;
   }
-  if (monitor() == nullptr) {
-    return;
-  }
-
   std::map<const char *, dev_depends_t>::const_iterator it =
                                                    kDevFuncDependsMap.begin();
   std::string dev_rt = path_ + "/device";
@@ -1151,7 +1147,9 @@ void Device::fillSupportedFuncs(void) {
 
     it++;
   }
-  monitor()->fillSupportedFuncs(&supported_funcs_);
+  if (monitor() != nullptr) {
+    monitor()->fillSupportedFuncs(&supported_funcs_);
+  }
   // DumpSupportedFunctions();
 }
 
